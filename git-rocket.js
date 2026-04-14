729b2347e865f467e3353fed8e96dc5e20c12a32
jarengomatc <jarengo@madisoncollege.edu>
2026-04-14 11:18:58 -0500

    Added my initials.

--- git-rocket.js
+++ git-rocket.js
@@ -6,7 +6,7 @@ export const rocket = [
   String.raw`           |  |           `,
   String.raw`          /|__|\          `,
   String.raw`         /  ||  \         `,
-  String.raw`        /   ||   \        `,
+  String.raw`        /   JA   \        `,
   String.raw`       |    ||    |       `,
   String.raw`       |    ||    |       `,
   String.raw`       |    ||    |       `,
