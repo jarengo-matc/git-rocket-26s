3b3cb51a2cb168f1da1ffdf5ba767199f565c11c
bpaulisse-matc <bpaulisse@madisoncollege.edu>
2026-04-14 11:19:03 -0500

    Added my initials

--- git-rocket.js
+++ git-rocket.js
@@ -6,7 +6,7 @@ export const rocket = [
   String.raw`           |  |           `,
   String.raw`          /|__|\          `,
   String.raw`         /  ||  \         `,
-  String.raw`        /   ||   \        `,
+  String.raw`        /   BP   \        `,
   String.raw`       |    ||    |       `,
   String.raw`       |    ||    |       `,
   String.raw`       |    ||    |       `,
