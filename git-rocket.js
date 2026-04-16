6bc3a3624dac0502871e53f82f9008f12977a617
tsteelematc <tsteele@madisoncollege.edu>
2026-04-16 10:49:00 -0500

    I added my initials to our rocket...

--- git-rocket.js
+++ git-rocket.js
@@ -6,7 +6,7 @@ export const rocket = [
   String.raw`           |  |           `,
   String.raw`          /|__|\          `,
   String.raw`         /  ||  \         `,
-  String.raw`        /   ||   \        `,
+  String.raw`        /   TS   \        `,
   String.raw`       |    ||    |       `,
   String.raw`       |    ||    |       `,
   String.raw`       |    ||    |       `,
